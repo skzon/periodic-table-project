734327637854c82841f5d4763b81335b69e3a793
suraj kumar <surajkumar2897@gmail.com>
2021-10-13 13:19:18 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PeriodicComponent } from './components/periodic/periodic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeriodicComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/periodic/periodic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-periodic',
+  templateUrl: './periodic.component.html',
+  styleUrls: ['./periodic.component.css']
+})
+export class PeriodicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
